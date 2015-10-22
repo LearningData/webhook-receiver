ac51532df2c2dc5bedede7eeaa075be063930d12
Edgar <me@edgarlatorre.com.br>
2015-10-22 12:11:06 +0100

    Changes to use configparser
    Removed repos dictionary and using configparser instead
    Removed Application class
    Created Repository class
    Added tests

--- receiver.py
+++ receiver.py
@@ -1,4 +1,6 @@
 import json
+import os
+from configparser import ConfigParser
 from flask import Flask, request
 app = Flask(__name__)
 
@@ -9,16 +11,45 @@ def update():
 
     if payload and "repository" in payload:
         if "name" in payload["repository"]:
-            repository = payload['repository']['name']
-            Application.update(respository)
+            name = payload['repository']['name']
+            repository = Repository(name)
+            if not repository.is_cloned():
+                repository.clone()
+
+            repository.update_mirror()
+
     return "ok"
 
 
-class Application:
-    @staticmethod
-    def update(repository):
-        pass
+class Repository:
+    origin = ""
+    mirror = ""
+
+    def __init__(self, name, config_file="repos.cfg"):
+        config = ConfigParser()
+        config.read(config_file)
+        self.name = name
+
+        if config.has_section(name):
+            self.origin = config.get(name, "origin")
+            self.mirror = config.get(name, "mirror")
+
+    def path(self):
+        return "/tmp/{}".format(self.name)
+
+    def is_cloned(self):
+        print(self.path())
+        return os.path.exists(self.path())
+
+    def clone(self):
+        os.system("cd {} && git clone {}".format("/tmp", self.origin))
+        os.system("cd {} && git remote add mirror {}".format(self.path(),
+                                                             self.mirror))
+
+    def update_mirror(self):
+        os.system("cd {} && git pull origin master".format(self.path()))
+        os.system("cd {} && git push mirror master".format(self.path()))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=4000)
--- tests.py
+++ tests.py
@@ -1,12 +1,23 @@
-import json
-import unittest
+from unittest import TestCase, mock, main
 import receiver
 
 
-class WebhookTest(unittest.TestCase):
+class WebhookTest(TestCase):
     def setUp(self):
         self.app = receiver.app.test_client()
-        self.data = '{"repository": {"name": "test"}}'
+        self.data = '{"repository": {\
+                        "id": 43555702,\
+                        "name": "name",\
+                        "full_name": "username/repository",\
+                        "owner": {\
+                            "name": "username",\
+                            "email": "test@test.com"\
+                        }}}'
+        self.patcher = mock.patch("receiver.Repository")
+        self.patcher.start()
+
+    def tearDown(self):
+        self.patcher.stop()
 
     def test_get_index_returns_ok(self):
         response = self.app.get("/")
@@ -16,6 +27,21 @@ class WebhookTest(unittest.TestCase):
         response = self.app.post('/',
                                  data=self.data,
                                  headers={'content-type': 'application/json'})
+        assert 200 == response.status_code
+
+
+class RepositoryTest(TestCase):
+    def setUp(self):
+        self.repository = receiver.Repository("sample")
+
+    def test_application_constructor_path(self):
+        assert self.repository.path() == "/tmp/sample"
+
+    def test_application_constructor_origin(self):
+        assert self.repository.origin == "git@sample.com:username/repo.git"
+
+    def test_application_constructor_mirror(self):
+        assert self.repository.mirror == "git@sample.com:username/mirror.git"
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
